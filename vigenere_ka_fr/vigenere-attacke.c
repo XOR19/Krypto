e80db38b6c22a9c699ff816cf13e375d8e4d898c
xor <xor@xor-VirtualBox>
2017-10-27 02:59:40 +0200

    better command line argument passing

--- vigenere_ka_fr/vigenere-attacke.c
+++ vigenere_ka_fr/vigenere-attacke.c
@@ -1,32 +1,28 @@
 /*************************************************************
-**         Europäisches Institut für Systemsicherheit        *
-**   Praktikum "Kryptographie und Datensicherheitstechnik"   *
-**                                                           *
-** Versuch 1: Klassische Chiffrierverfahren                  *
-**                                                           *
-**************************************************************
-**
-** vigenere_attacke.c: Brechen der Vigenere-Chiffre
-**/
-
-
-#define ASSERT(xx)
+ **         Europäisches Institut für Systemsicherheit        *
+ **   Praktikum "Kryptographie und Datensicherheitstechnik"   *
+ **                                                           *
+ ** Versuch 1: Klassische Chiffrierverfahren                  *
+ **                                                           *
+ **************************************************************
+ **
+ ** vigenere_attacke.c: Brechen der Vigenere-Chiffre
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <praktikum.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <linux/limits.h>
 
 #define NUMCHARS    26       /* Anzahl der Zeichenm, die betrachtet werden ('A' .. 'Z') */
-#define MaxFileLen  32768    /* Maximale Größe des zu entschlüsselnden Textes */
 
-const char *StatisticFileName = "statistik.data";  /* Filename der Wahrscheinlichkeitstabelle */
-const char *WorkFile          = "testtext.ciph";   /* Filename des verschlüsselten Textes */
+const char *StatisticFileName = "statistik.data"; /* Filename der Wahrscheinlichkeitstabelle */
+const char *WorkFile = "testtext.ciph"; /* Filename des verschlüsselten Textes */
 
 double PropTable[NUMCHARS]; /* Tabellke mit den Zeichenwahrscheinlichkeiten.
-			     * ProbTable[0] == 'A', PropTable[1] == 'B' usw. */
-char TextArray[MaxFileLen]; /* die eingelesene Datei */
-int TextLength;             /* Anzahl der gültigen Zeichen in TextArray */
+ * ProbTable[0] == 'A', PropTable[1] == 'B' usw. */
 
 /*--------------------------------------------------------------------------*/
 
@@ -35,29 +31,29 @@ int TextLength;             /* Anzahl der gültigen Zeichen in TextArray */
  * STATISTICFILENAME in das globale Array PROPTABLE ein.
  */
 
-static void GetStatisticTable(void)
-  {
-    FILE *inp;
-    int i;
-    char line[64];
+static void GetStatisticTable(const char* file) {
+	FILE *inp;
+	int i;
+	char line[64];
 
-    if (!(inp=fopen(StatisticFileName,"r"))) {
-      fprintf(stderr,"FEHLER: File %s kann nicht geöffnet werden: %s\n",
-	      StatisticFileName,strerror(errno));
-      exit(20);
-    }
+	if (!(inp = fopen(file, "r"))) {
+		fprintf(stderr, "FEHLER: File %s kann nicht geöffnet werden: %s\n",
+				StatisticFileName, strerror(errno));
+		exit(20);
+	}
 
-    for (i=0; i<TABSIZE(PropTable); i++) {
-      fgets(line,sizeof(line),inp);
-      if (feof(inp)) {
-        fprintf(stderr,"FEHLER: Unerwartetes Dateieine in %s nach %d Einträgen.\n",
-		StatisticFileName,i);
-	exit(20);
-      }
-      PropTable[i] = atof(line);
-    }
-    fclose(inp);
-  }
+	for (i = 0; i < TABSIZE(PropTable); i++) {
+		fgets(line, sizeof(line), inp);
+		if (feof(inp)) {
+			fprintf(stderr,
+					"FEHLER: Unerwartetes Dateieine in %s nach %d Einträgen.\n",
+					StatisticFileName, i);
+			exit(20);
+		}
+		PropTable[i] = atof(line);
+	}
+	fclose(inp);
+}
 
 /*-------------------------------------------------------------------------*/
 
@@ -68,33 +64,44 @@ static void GetStatisticTable(void)
  *   Kleinbuchstaben werden beim Einlesen in Großbuchstaben gewandelt.
  */
 
-static void GetFile(void)
-  {
-    FILE *inp;
-    char c;
+static char* GetFile(const char* file, int* length) {
+	FILE *inp;
+	char c;
 
-    if (!(inp=fopen(WorkFile,"r"))) {
-      fprintf(stderr,"FEHLER: File %s kann nicht geöffnet werden: %s\n",
-	      WorkFile,strerror(errno));
-      exit(20);
-    }
-
-    TextLength=0;
-    while (!feof(inp)) {
-      c = fgetc(inp);
-      if (feof(inp)) break;
-      if (c>='a' && c<='z') c -= 32;
-      if (c>='A' && c<='Z') {
-	if (TextLength >= sizeof(TextArray)) {
-	  fprintf(stderr,"FEHLER: Eingabepuffer nach %d Zeichen übergelaufen!\n",TextLength);
-	  exit(20);
+	if (!(inp = fopen(file, "r"))) {
+		fprintf(stderr, "FEHLER: File %s kann nicht geöffnet werden: %s\n",
+				WorkFile, strerror(errno));
+		exit(20);
 	}
-        TextArray[TextLength++] = c;
-      }
-    }
-    fclose(inp);
-  }
 
+	fseek(inp, 0L, SEEK_END);
+	long int size = ftell(inp);
+	fseek(inp, 0L, SEEK_SET);
+
+	if (size > 1048576)
+		size = 1048576;
+	char* buf = (char*) malloc(size + 1);
+	if (!buf) {
+		fprintf(stderr, "FEHLER: Buffer konnte nicht allokiert werden\n");
+		exit(20);
+	}
+	int i = 0;
+	while (!feof(inp) && i < size) {
+		c = fgetc(inp);
+		if (feof(inp))
+			break;
+		if (c >= 'a' && c <= 'z')
+			c -= 32;
+		if (c >= 'A' && c <= 'Z') {
+			buf[i++] = c;
+		}
+	}
+	fclose(inp);
+	buf[i] = 0;
+	if (length)
+		*length = i;
+	return buf;
+}
 
 /*--------------------------------------------------------------------------*/
 
@@ -113,104 +120,175 @@ static void GetFile(void)
  * Man beachte, daß das erste Zeichen eines C-Strings den Offset 0 besitzt!
  */
 
-static void CountChars( int start, int offset, int h[NUMCHARS])
-  {
-    int i;
-    char c;
+static void CountChars(const char* text, int textLength, int start, int offset,
+		int h[NUMCHARS]) {
+	int i;
+	char c;
 
-    for (i=0; i<NUMCHARS; i++) h[i] = 0;
+	for (i = 0; i < NUMCHARS; i++)
+		h[i] = 0;
 
-    /*****************  Aufgabe  *****************/
-    for(i=start; i<TextLength && i>=0; i+=offset){
-    	c = TextArray[i];
-    	ASSERT(c>='A' && c<='Z')
-    	h[c-'A']++;
-    }
-  }
+	/*****************  Aufgabe  *****************/
+	for (i = start; i < textLength && i >= 0; i += offset) {
+		c = text[i];
+		h[c - 'A']++;
+	}
+}
 
-
-
-static int GetKey(int h[NUMCHARS], int n, double* conf){
+static int GetKey(int h[NUMCHARS], int n, double* conf) {
 	int i;
 	int off;
 	int min = -1;
 	double min_err = 1e100;
-	for(off=0; off<NUMCHARS; off++){
+	for (off = 0; off < NUMCHARS; off++) {
 		double err = 0;
-		for(i=0; i<NUMCHARS; i++){
-			double e = h[(i+off)%NUMCHARS];
+		for (i = 0; i < NUMCHARS; i++) {
+			double e = h[(i + off) % NUMCHARS];
 			e /= n;
 			e -= PropTable[i];
-			err += e*e; // Quadratischer fehler minimieren
+			err += e * e; // Quadratischer fehler minimieren
 		}
-		if(err<min_err){
+		if (err < min_err) {
 			min_err = err;
 			min = off;
 		}
 	}
-	if(conf)
+	if (conf)
 		*conf = min_err;
-	return min==0?'Z':min-1+'A';
+	return min == 0 ? 'Z' : min - 1 + 'A';
 }
 
+static void printhelp(const char* cmd) {
+	printf("Aufruf: %s [OPTION]... INPUT [OUTPUT]\n", cmd);
+	printf("Optionen:\n");
+	printf("  -h, --help        Diese Hilfe ausgeben und beenden\n");
+	printf("  -v, --version     Versionsnummer ausgeben und beenden\n");
+	printf("  -s, --statistics  Statistics file\n");
+	printf("\n");
+	printf(
+			"Ist OUTPUT nicht spezifiziert wird die Ausgabe auf stdout umgeleitet,\n");
+	printf("ist INPUT nicht spezifiziert wird testtext.ciph angenommen.\n");
+}
 
+static void printshorthelp(const char* cmd) {
+	printf("Aufruf: %s [OPTION]... INPUT [OUTPUT]\n", cmd);
+	printf("„%s --help“ liefert weitere Informationen.\n", cmd);
+}
+
+static void printversion(void) {
+	printf("vigenere-attacke 1.0\n");
+}
+
+const static struct option long_options[] = {
+		{ "version", no_argument, 0, 'v' }, { "help", no_argument, 0, 'h' }, {
+				"statistics", required_argument, 0, 's' }, { 0, 0, 0, 0 } };
 /*------------------------------------------------------------------------------*/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
-  GetStatisticTable();     /* Wahrscheinlichkeiten einlesen */
-  GetFile();               /* zu bearbeitendes File einlesen */
+	char statisticFileName[PATH_MAX];
+	char infile[PATH_MAX];
+	char outfile[PATH_MAX];
+	strcpy(statisticFileName, "statistik.data");
 
-  /*****************  Aufgabe  *****************/
-  int h[NUMCHARS];
-  CountChars(0, 1, h);
-  double I_c = 0;
-  int i;
-  for(i=0; i<NUMCHARS; i++){
-	  double v = h[i]/(double)TextLength;
-	  I_c += v*v;
-  }
-  double I_c_rand = 1.0/NUMCHARS;
-  double I_c_eng = 0.065;
+	int opt;
+	int option_index;
+	while ((opt = getopt_long(argc, argv, "hH?vVs:", long_options,
+			&option_index)) != -1) {
 
-  double v = (I_c-I_c_rand)/(I_c_eng-I_c_rand); //==(n-l)/(l(n-1))
-  double l = TextLength/(v*(TextLength+1)+1);
-  
-  printf("l: %f\n", l);
-  
-  int roundedL = (int)(l+0.5);
-  int curr = roundedL;
-  int next = curr+1;
-  int up = -1;
-  char keybuf[TextLength+1];
-  
-  while(next>=0 && curr<=TextLength){
-	  double conf;
-	  double confm = 0;
-	  for(i=0; i<curr; i++){
-		  CountChars(i, curr, h);
-		  int c = GetKey(h, TextLength/curr, &conf);
-		  confm += conf;
-		  keybuf[i] = c;
-	  }
-	  keybuf[curr] = 0;
-	  confm /= curr;
-	  printf("Key for len %i with error %f:%s\n", curr, confm, keybuf);
-	  if(confm<0.015){ // Can be done better?
-		  printf("Likely Key:%s\n", keybuf);
-		  execl("vigenere", "vigenere", WorkFile, "testtext.crack", keybuf, "decipher", NULL);
-		  return 0;
-	  }
-	  
-	  int tmp = curr;
-	  curr = next;
-	  next = tmp;
-	  next += up;
-	  up = -up;
-  }
-  
-  printf("nothing found\n");
+		switch (opt) {
+		case 'h':
+		case 'H':
+		case '?':
+			printhelp(argv[0]);
+			exit(0);
+		case 'v':
+		case 'V':
+			printversion();
+			exit(0);
+		case 's':
+			strncpy(statisticFileName, optarg, sizeof(statisticFileName));
+			break;
+		}
 
-  return 0;
+	}
+
+	const char* outparam;
+
+	if (optind < argc) {
+		strncpy(infile, argv[optind++], sizeof(infile));
+	} else {
+		strncpy(infile, WorkFile, sizeof(infile));
+	}
+
+	if (optind < argc) {
+		outparam = outfile;
+		strncpy(outfile, argv[optind++], sizeof(outfile));
+	} else {
+		outparam = 0;
+	}
+
+	if (optind < argc) {
+		printf("Too many arguments\n");
+		printshorthelp(argv[0]);
+		exit(1);
+	}
+
+	GetStatisticTable(statisticFileName);/* Wahrscheinlichkeiten einlesen */
+
+	int length;
+	char* data = GetFile(infile, &length); /* zu bearbeitendes File einlesen */
+
+	/*****************  Aufgabe  *****************/
+	int h[NUMCHARS];
+	CountChars(data, length, 0, 1, h);
+	double I_c = 0;
+	int i;
+	for (i = 0; i < NUMCHARS; i++) {
+		double v = h[i] / (double) length;
+		I_c += v * v;
+	}
+	double I_c_rand = 1.0 / NUMCHARS;
+	double I_c_eng = 0.065;
+
+	double v = (I_c - I_c_rand) / (I_c_eng - I_c_rand); //==(n-l)/(l(n-1))
+	double l = length / (v * (length + 1) + 1);
+
+	printf("l: %f\n", l);
+
+	int roundedL = (int) (l + 0.5);
+	int curr = roundedL;
+	int next = curr + 1;
+	int up = -1;
+	char keybuf[length + 1];
+
+	while (next >= 0 && curr <= length) {
+		double conf;
+		double confm = 0;
+		for (i = 0; i < curr; i++) {
+			CountChars(data, length, i, curr, h);
+			int c = GetKey(h, length / curr, &conf);
+			confm += conf;
+			keybuf[i] = c;
+		}
+		keybuf[curr] = 0;
+		confm /= curr;
+		printf("Key for len %i with error %f:%s\n", curr, confm, keybuf);
+		if (confm < 0.015) { // Can be done better?
+			free(data);
+			printf("Likely Key:%s\n", keybuf);
+			execl("vigenere", "vigenere", "d", keybuf, infile, outparam, NULL);
+			return 0;
+		}
+
+		int tmp = curr;
+		curr = next;
+		next = tmp;
+		next += up;
+		up = -up;
+	}
+	free(data);
+	printf("nothing found\n");
+
+	return 0;
 }
--- vigenere_ka_fr/vigenere.c
+++ vigenere_ka_fr/vigenere.c
@@ -1,19 +1,21 @@
 /*************************************************************
-**         Europäisches Institut für Systemsicherheit        *
-**   Praktikum "Kryptographie und Datensicherheitstechnik"   *
-**                                                           *
-** Versuch 1: Klassische Chiffrierverfahren                  *
-**                                                           *
-**************************************************************
-**
-** vigenere.c: Implementierung einer Vigenere-Chiffre
-**/
+ **         Europäisches Institut für Systemsicherheit        *
+ **   Praktikum "Kryptographie und Datensicherheitstechnik"   *
+ **                                                           *
+ ** Versuch 1: Klassische Chiffrierverfahren                  *
+ **                                                           *
+ **************************************************************
+ **
+ ** vigenere.c: Implementierung einer Vigenere-Chiffre
+ **/
 
 #define INC_KEY_ON_IGNORE 0
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <linux/limits.h>
+#include <getopt.h>
 
 #define MODE_ENCIPHER 0
 #define MODE_DECIPHER 1
@@ -23,133 +25,88 @@
 
 #include <praktikum.h>
 
-/*********************  Globale Hilfsbvariabeln  *******************/
-String Key;     /* Schlüssel */
-int keyPos;
-
-/*
- * int Encipher(int c) : Interpretiert C als Zeichen, verschlüsselt es nach der
- *                       Methode von Vigenere und gibt das Ergebnis als Resultat
- *                       zurück.
- */
-
-static int Encipher(int c) {
-
-#if !INC_KEY_ON_IGNORE
-	if(c>='A' && c<='Z') {
-#endif
-
-		int keyC = Key[keyPos++];
-		if(!keyC){
-			keyC = Key[0];
-			keyPos = 1;
-		}
-
-#if INC_KEY_ON_IGNORE
-	if(c>='A' && c<='Z') {
-#endif
-
-		int move = (keyC - 'A' + 1)%26; // 'Z' würde probleme machen
-
-		c += move;
-		if(c>'Z')
-			c -= 26;  // 'Z' - 'A'
-	}
-
-	return c;
-  }
-
-
-/*
- * int Decipher(int c) : Interpretiert C als Zeichen, entschlüsselt es nach der
- *                       Methode von Vigenere und gibt das Ergebnis als Resultat
- *                       zurück.
- */
-
-static int Decipher(int c) {
-
-#if !INC_KEY_ON_IGNORE
-	if(c>='A' && c<='Z') {
-#endif
-
-		int keyC = Key[keyPos++];
-		if(!keyC){
-			keyC = Key[0];
-			keyPos = 1;
-		}
-
-#if INC_KEY_ON_IGNORE
-	if(c>='A' && c<='Z') {
-#endif
-
-		int move = (keyC - 'A' + 1)%26; // 'Z' würde probleme machen
-
-		c -= move;
-		if(c<'A')
-			c += 26;  // 'Z' - 'A'
-
-	}
-
-	return c;
-  }
-  
-static unsigned int mathMod(int n, unsigned int mod) {
-    n %= mod;
-    return n < 0 ? n + mod : n;
-}
-
 static inline bool isCharValid(char c) {
-    return c <= 'Z' && c >= 'A';
+	return c <= 'Z' && c >= 'A';
 }
 
-static inline char charToSymbol(char c) {
-    return c - 'A' + 1;
+static void prepareKey(const char* keyIn, char* keyOut, unsigned char mode) {
+	while (*keyIn) {
+		char c = *keyIn - 'A' + 1; // 1<=c<=NUMCHARS
+		*keyOut = mode == MODE_DECIPHER ? NUMCHARS - c : c % NUMCHARS;
+		keyIn++;
+		keyOut++;
+	}
+	// 0<=keyOut[i]<NUMCHARS
 }
 
-static inline char symbolToChar(char c) {
-    return c + 'A' - 1;
-}
+static int vigenere(const char *bufIn, char *bufOut, unsigned int bufLength,
+		const char *shifts, unsigned int shiftsLength,
+		unsigned int initShiftsIdx) {
 
-static void vigenere(const char *bufIn, char *bufOut, unsigned int bufLength, const char *key, unsigned int initKeyIdx, unsigned char mode) {
-    int keyLength = strlen(key);
-    char *shifts = (char *) malloc(keyLength * sizeof(char));
-    
-    for (int i = 0; i < keyLength; ++i) {
-        // the key symbols have a sign
-        shifts[i] = mode == MODE_DECIPHER ? -charToSymbol(key[i]) : charToSymbol(key[i]);
-    }
-    
-    char c, symbol;
-    for (unsigned int i = 0, idx = initKeyIdx; i < bufLength; ++i) {
-        if (!isCharValid(c = bufIn[i])) {
-            bufOut[i] = bufIn[i]; 
-            
-            #if INC_KEY_ON_IGNORE 
-                ++idx; // only advance on valid symbols except when INC_KEY_ON_IGNORE ist true
-            #endif
-            
-            continue;
-        }
-        
-        if (idx >= keyLength) {
-            idx = 0;
-        }
-        
-        symbol = charToSymbol(c);
-        bufOut[i] = symbolToChar(mathMod(symbol + shifts[idx++], NUMCHARS));
-    } 
-    free(shifts);
+	char c, symbol;
+	unsigned int i, idx;
+	for (i = 0, idx = initShiftsIdx; i < bufLength; ++i) {
+		if (!isCharValid(c = bufIn[i])) {
+			bufOut[i] = c;
+
+#if INC_KEY_ON_IGNORE
+			++idx; // only advance on valid symbols except when INC_KEY_ON_IGNORE ist true
+#endif
+
+			continue;
+		}
+
+		if (idx >= shiftsLength) {
+			idx = 0;
+		}
+
+		symbol = c - 'A';
+		bufOut[i] = ((symbol + shifts[idx++]) % NUMCHARS) + 'A';
+	}
+	return idx;
 }
 
 static bool stringValid(char *str) {
-    for (char *c = str; *c; ++c) {
-        if (!isCharValid(*c)) {
-            return false;
-        }
-    }
-    return true;
+	for (char *c = str; *c; ++c) {
+		if (!isCharValid(*c)) {
+			return false;
+		}
+	}
+	return true;
 }
 
+static void printhelp(const char* cmd) {
+	printf("Aufruf: %s [OPTION]... MODE KEY INPUT [OUTPUT]\n", cmd);
+	printf("Für Compatiblitaet: %s [OPTION]... [INPUT] [OUTPUT] [KEY] [MODE]\n",
+			cmd);
+	printf("Optionen:\n");
+	printf("  -h, --help     Diese Hilfe ausgeben und beenden\n");
+	printf("  -v, --version  Versionsnummer ausgeben und beenden\n");
+	printf("\n");
+	printf("Mode:\n");
+	printf("   e, encipher   Entschlüsseln\n");
+	printf("   d, decipher   Verschlüsseln\n");
+	printf("\n");
+	printf(
+			"Ist OUTPUT nicht spezifiziert wird die Ausgabe auf stdout umgeleitet,\n");
+	printf(
+			"Sind INPUT, OUTPUT, KEY, MODE in compatiblitaet nicht spezifiziert, \n");
+	printf("wird der user per promt nach den daten gefragt.\n");
+}
+
+static void printshorthelp(const char* cmd) {
+	printf("Aufruf: %s [OPTION]... MODE INPUT [OUTPUT]\n", cmd);
+	printf("„%s --help“ liefert weitere Informationen.\n", cmd);
+}
+
+static void printversion(void) {
+	printf("vigenere 1.0\n");
+}
+
+const static struct option long_options[] = {
+		{ "version", no_argument, 0, 'v' },
+		{ "help", no_argument, 0, 'h' },
+		{0, 0, 0, 0 } };
 
 /*
  * main(argc,argv) : Das Hauptprogramm, welches beim Aufruf von VIGENERE aufgerufen wird.
@@ -158,122 +115,177 @@ static bool stringValid(char *str) {
  *   ARGV[0] enthält den Namen des Programms.
  */
 
-int main(int argc, char **argv)
-{
-  String infilename, outfilename, help, zeile;
-  int mode;
-  /***** weitere (lokale) Hiflsbvariabeln *******/
+int main(int argc, char **argv) {
+	char infilename[PATH_MAX];
+	char outfilename[PATH_MAX];
+	String help, key;
+	int mode;
+	int outtostd = 0;
+	/***** weitere (lokale) Hiflsbvariabeln *******/
 
-  FILE *infile, *outfile;
+	FILE *infile, *outfile;
 
-  /* Wenn die Ein- bzw. Ausgabedatei oder der Schlüssel nicht in der
-   * Kommandozeile angegeben wurden, fragen wir einfach nach .... */
-  if (argc<2) {
-      readline("Eingabefile : ", infilename, sizeof(infilename));
-  } else {
-      strncpy(infilename, argv[1], sizeof(infilename));
-  } if (argc<3) {
-      readline("Ausgabefile : ", outfilename, sizeof(outfilename));
-  } else {
-      strncpy(outfilename, argv[2], sizeof(outfilename));
-  } if (argc<4) {
-      readline("Schluessel  : ", Key,sizeof(Key));      
-  } else {
-      strncpy(Key, argv[3], sizeof(Key));
-  }
+	int opt;
+	int option_index;
+	while ((opt = getopt_long(argc, argv, "hH?vV", long_options, &option_index))
+			!= -1) {
 
-  if (argc<5) {
-    do {
-      readline("V)er- oder E)ntschlüsseln : ",help,sizeof(help));
-      string_to_upper(help);
-    }
-    while (strlen(help)!=1 && help[0]!='V' && help[0]!='E');
-    mode = help[0]=='E';
-  } else {
-    if (!strncmp(argv[4],"encipher",strlen(argv[4]))) {
-        mode = 0;
-    } else if (!strncmp(argv[4],"decipher",strlen(argv[4]))) {
-        mode = 1;
-    } else {
-      fprintf(stderr,"FEHLER: Unbekannter Modus, 'encipher' oder 'decipher' erwartet.\n");
-      exit(20);
-    }
-  }
-  string_to_upper(Key);
-  
-  unsigned int keyLength = strlen(Key);
-  if (!stringValid(Key) || keyLength == 0) {
-      fprintf(stderr, "FEHLER: Ungueltiger Schluessel.\n");
-      exit(0xBAD);
-  }
+		switch (opt) {
+		case 'h':
+		case 'H':
+		case '?':
+			printhelp(argv[0]);
+			exit(0);
+		case 'v':
+		case 'V':
+			printversion();
+			exit(0);
+		}
 
-  /* Öffnen der Dateien:
-   *  `fopen' gibt im Fehlerfall einen NULL-Pointer zurück. Kann die Datei
-   *  geöffnet werden, so wird der von `fopen' zurückgelieferte FILE-Pointer
-   *  als Argument bei den Aufrufen `fgets', `fprintf', `fclose' usw. 
-   *  zur Identifizierung der Datei angegeben.
-   */
-  if (!(infile=fopen(infilename,"r"))) {
-    fprintf(stderr,"FEHLER: Eingabefile %s kann nicht geöffnet werden: %s\n",infilename,strerror(errno));
-    exit(20);
-  }
-  
-  if (!(outfile=fopen(outfilename,"w"))) {
-    fprintf(stderr,"FEHLER: Ausgabefile %s kann nicht geöffnet werden: %s\n",outfilename,strerror(errno));
-    exit(20);
-  }
+	}
 
-  /* Belegung der Variablen:
-   *  infilename : Name der Eingabedatei 
-   * outfilename : Name der Ausgabedatei
-   *      infile : `Datei-Bezeichner', der die Eingabedatei repräsentiert.
-   *     outfile : `Datei-Bezeichner', der die Ausgabedatei repräsentiert.
-   *         Key : Schlüssel, nach Großschrift gewandelt
-   *        mode : Flag, == 1 im Entschlüsselungsmodus, ansonsten 0.
-   */
+	mode = -1;
+	if (optind < argc) {
+		const char* mode_arg = argv[optind];
+		if (mode_arg[0] == 'd') {
+			if (mode_arg[1] == 0 || !strcmp(mode_arg, "decipher")) {
+				mode = MODE_DECIPHER;
+			}
+		} else if (mode_arg[0] == 'e') {
+			if (mode_arg[1] == 0 || !strcmp(mode_arg, "encipher")) {
+				mode = MODE_ENCIPHER;
+			}
+		}
+		if (mode != -1)
+			optind++;
+	}
+	if (mode == -1) {
+		// old compatiblity
+		if (optind < argc) {
+			strncpy(infilename, argv[optind++], sizeof(infilename));
+		} else {
+			readline("Eingabefile : ", infilename, sizeof(infilename));
+		}
+		if (optind < argc) {
+			strncpy(outfilename, argv[optind++], sizeof(outfilename));
+		} else {
+			readline("Ausgabefile : ", outfilename, sizeof(outfilename));
+		}
+		if (optind < argc) {
+			strncpy(key, argv[optind++], sizeof(key));
+		} else {
+			readline("Schluessel  : ", key, sizeof(key));
+		}
+		if (optind < argc) {
+			const char* mode_arg = argv[optind++];
+			if (mode_arg[0] == 'e'
+					&& (mode_arg[1] == 0 || !strcmp(mode_arg, "encipher"))) {
+				mode = MODE_ENCIPHER;
+			} else if (mode_arg[0] == 'd'
+					&& (mode_arg[1] == 0 || !strcmp(mode_arg, "decipher"))) {
+				mode = MODE_DECIPHER;
+			} else {
+				fprintf(stderr,
+						"FEHLER: Unbekannter Modus, 'encipher' oder 'decipher' erwartet.\n");
+				exit(20);
+			}
+		} else {
+			do {
+				readline("V)er- oder E)ntschlüsseln : ", help, sizeof(help));
+				string_to_upper(help);
+			} while (strlen(help) != 1 && help[0] != 'V' && help[0] != 'E');
+			mode = help[0] == 'E' ? MODE_ENCIPHER : MODE_DECIPHER;
+		}
+	} else {
+		if (optind >= argc) {
+			printf("KEY expected\n");
+			printshorthelp(argv[0]);
+			exit(1);
+		}
+		strncpy(key, argv[optind++], sizeof(key));
+		if (optind >= argc) {
+			printf("INPUT expected\n");
+			printshorthelp(argv[0]);
+			exit(1);
+		}
+		strncpy(infilename, argv[optind++], sizeof(infilename));
+		if (optind < argc) {
+			strncpy(outfilename, argv[optind++], sizeof(outfilename));
+		} else {
+			outtostd = 1;
+		}
+		if (optind < argc) {
+			printf("Too many arguments\n");
+			printshorthelp(argv[0]);
+			exit(1);
+		}
+	}
 
-  //*
-  keyPos = 0;
-  do {
-    fgets(zeile,sizeof(zeile),infile);
-    if (!feof(infile)) {
-      strip_crlf(zeile);
-      string_to_upper(zeile);
+	string_to_upper(key);
 
-      char* ptr = zeile;
-      while(*ptr){
-    	  *ptr = mode?Decipher(*ptr):Encipher(*ptr);
-    	  ptr++;
-      }
+	unsigned int keyLength = strlen(key);
+	if (!stringValid(key) || keyLength == 0) {
+		fprintf(stderr, "FEHLER: Ungueltiger Schluessel.\n");
+		exit(0xBAD);
+	}
 
-      fprintf(outfile,"%s\n",zeile);
-    }
-  }
-  while (!feof(infile));
-  //*/
-  
-  /*
-  char bufferIn[BLOCK_SIZE + 1];
-  char bufferOut[BLOCK_SIZE];
-  
-  bufferIn[BLOCK_SIZE] = 0; // null-terminate it so that it is also a C-string
-  
-  size_t charsRead;
-  unsigned int keyIndex = 0;
-  do {
-      charsRead = fread(bufferIn, 1, BLOCK_SIZE, infile);
-      string_to_upper(bufferIn);
-      
-      vigenere(bufferIn, bufferOut, charsRead, Key, keyIndex, mode);
-      fwrite(bufferOut, 1, charsRead, outfile);
-      
-      keyIndex = (keyIndex + BLOCK_SIZE) % keyLength;
-  } while (charsRead == BLOCK_SIZE);
-  //*/
-  
-  /* Schließen der Ein- und Ausgabedateien */
-  fclose(infile);
-  fclose(outfile);
+	/* Öffnen der Dateien:
+	 *  `fopen' gibt im Fehlerfall einen NULL-Pointer zurück. Kann die Datei
+	 *  geöffnet werden, so wird der von `fopen' zurückgelieferte FILE-Pointer
+	 *  als Argument bei den Aufrufen `fgets', `fprintf', `fclose' usw.
+	 *  zur Identifizierung der Datei angegeben.
+	 */
+	if (!(infile = fopen(infilename, "r"))) {
+		fprintf(stderr,
+				"FEHLER: Eingabefile %s kann nicht geöffnet werden: %s\n",
+				infilename, strerror(errno));
+		exit(20);
+	}
 
-  return 0;
+	if (outtostd) {
+		outfile = stdout;
+	} else {
+		if (!(outfile = fopen(outfilename, "w"))) {
+			fprintf(stderr,
+					"FEHLER: Ausgabefile %s kann nicht geöffnet werden: %s\n",
+					outfilename, strerror(errno));
+			exit(20);
+		}
+	}
+
+	/* Belegung der Variablen:
+	 *  infilename : Name der Eingabedatei
+	 * outfilename : Name der Ausgabedatei
+	 *      infile : `Datei-Bezeichner', der die Eingabedatei repräsentiert.
+	 *     outfile : `Datei-Bezeichner', der die Ausgabedatei repräsentiert.
+	 *         Key : Schlüssel, nach Großschrift gewandelt
+	 *        mode : Flag, == 1 im Entschlüsselungsmodus, ansonsten 0.
+	 */
+
+	char bufferIn[BLOCK_SIZE + 1];
+
+	bufferIn[BLOCK_SIZE] = 0; // null-terminate it so that it is also a C-string
+	// even if not needed
+
+	size_t charsRead;
+	unsigned int keyIndex = 0;
+
+	prepareKey(key, key, mode);
+
+	do {
+		charsRead = fread(bufferIn, 1, BLOCK_SIZE, infile);
+		string_to_upper(bufferIn);
+
+		keyIndex = vigenere(bufferIn, bufferIn, charsRead, key, keyLength,
+				keyIndex);
+		fwrite(bufferIn, 1, charsRead, outfile);
+
+	} while (charsRead == BLOCK_SIZE);
+
+	/* Schließen der Ein- und Ausgabedateien */
+	fclose(infile);
+	if (!outtostd)
+		fclose(outfile);
+
+	return 0;
 }
