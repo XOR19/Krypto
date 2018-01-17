58b55427cf975dd5de8750de6cf14ba1729140c7
xor <xor@xor-VirtualBox>
2018-01-17 20:16:05 +0100

    First version, but for some reason not working, getting allways pw#41

--- breach/attack.c
+++ breach/attack.c
@@ -10,15 +10,18 @@
 
 #include "breach.h"
 
+#define USER_NAME "cr4ck1411"
+
 Connection con;
 
-void enc(const char *guess, struct Reply *rpl) {
+static void enc(const char *guess, struct Reply *rpl) {
 	/* get the encrypted message from the daemon */
 	struct Message msg;
 	int cnt;
+	memset(&msg, 0, sizeof(msg));
 	msg.Type = Guess;
-	assert(strlen(guess) < sizeof(((struct Message *) 0)->Guess));
-	strcpy(msg.Guess, guess);
+	assert(strlen(guess) < sizeof(msg.Guess));
+	strncpy(msg.Guess, guess, sizeof(msg.Guess));
 	cnt = Transmit(con, &msg, sizeof(msg));
 	if(cnt != sizeof(msg)) {
 		printf("Failed to transmit message.\n");
@@ -31,41 +34,93 @@ void enc(const char *guess, struct Reply *rpl) {
 	}
 }
 
-int attack(void) {
+static void printhexdata(const void*d, int len) {
+	const char* c = (const char*)d;
+	while (len-- > 0) {
+		printf("%02x", *c++);
+	}
+}
+
+static int attack(void) {
 	struct Reply rpl;
-	enc("wabbajack", &rpl);
-	/*>>>>         <<<<*
-	 *>>>> AUFGABE <<<<*
-	 *>>>>         <<<<*/
+	char password[100] = {0};
+	char try[11] = "^pw: ";
+	int len = 5;
+	int pos = 0;
+	int l;
+	char fc;
+	while(1){
+		char c;
+		if(len<10){
+			try[len] = ' ';
+			try[len+1] = '^';
+		}else{
+			try[9] = ' ';
+		}
+		enc(try, &rpl);
+		l = rpl.Len;
+		fc = ' ';
+		for(c='a'; c<='z'; c++){
+			if(len<10){
+				try[len] = c;
+			}else{
+				try[9] = c;
+			}
+			enc(try, &rpl);
+			//printf("Try: %s %d\n", try, rpl.Len);
+			if(rpl.Len<l){
+				l = rpl.Len;
+				fc = c;
+			}else if(rpl.Len==l){
+				fc = 0;
+			}
+		}
+		if(fc==0){
+			printf("Problems\n");
+			return 0;
+		}else if(fc==' ')
+			break;
+		char b[2];
+		b[0] = fc;
+		b[1] = 0;
+		//printf("Next: %s %d\n", b, pos);
+		if(len<10){
+			try[len] = fc;
+		}else{
+			memcpy(try+1, try+2, 7);
+			try[8] = fc;
+		}
+		password[pos] = fc;
+		pos++;
+		len++;
+	}
+	enc(password, &rpl);
+	printhexdata(&rpl, sizeof(rpl));
+	printf("\n");
 	if(rpl.Type == Correct) {
-		printf("Password is \"wabbajack\" (%d bytes total message size)\n", rpl.Len);
+		printf("Password is \"%s\" (%d bytes total message size)\n", password, rpl.Len);
 		return 1;
 	} else {
-		printf("Password not found\n");
+		printf("Password \"%s\" len %d wrong (%d bytes total message size)\n", password, pos, rpl.Len);
 		return 0;
 	}
 }
 
 int main(int argc, char *argv[]) {
 	/* initiate communication with daemon */
-	char *us = MakeNetName("");
-	char *them = "breach"; /* mind the ro */
-	char *tmp;
+	const char *us = USER_NAME;
+	const char *them = "breach"; /* mind the ro */
 	if(!(con = ConnectTo((const char *) us, (const char *) them))) {
 		printf("Failed to get daemon's attention: %s\n", NET_ErrorText());
 		return 0;
 	}
 	DisConnect(con);
-	tmp = us;
-	them = concatstrings("breach_", us, NULL);
-	us = concatstrings(us, "_breach", NULL);
-	free(tmp);
+	them = "breach_" USER_NAME;
+	us = USER_NAME "_breach";
 	if(!(con = ConnectTo((const char *) us, (const char *) them))) {
 		printf("Failed to open channel with daemon: %s\n", NET_ErrorText());
 		return 0;
 	}
-	free(us);
-	free(them);
 	int result = !attack();
 	DisConnect(con);
 	return result;
